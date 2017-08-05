4b9be23a9ea8c333b1262fc0b9d0ec92a4801974
ciazhar <daisetsunakama@gmail.com>
2017-08-05 07:51:23 +0700

    start project

---
+++ src/main/java/com/ciazhar/configservicegitrepository/configservicegitrepository/ConfigServiceGitRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.ciazhar.configservicegitrepository.configservicegitrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceGitRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceGitRepositoryApplication.class, args);
+	}
+}
